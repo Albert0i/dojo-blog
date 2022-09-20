0e4f923dea36a4d8bf010e4d326becfa3af87a88
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-09-20 17:13:25 +0800

    EOD

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
     const { data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
-
+    const history = useHistory()
+    
+    const handleEdit = () => {
+        console.log('edit')
+    }
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(()=>{
+            console.log('blog deleted')
+            //history.go(-1)
+            history.push('/')
+          })
+    }
     return ( 
         <div className="blog-details">
             { error && <div><h1>{ error }</h1></div> }
@@ -14,6 +28,8 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button className="blog-details-edit" onClick={handleEdit}>edit</button>
+                    <button className="blog-details-delete" onClick={handleDelete}>delete</button>
                 </article>
             ) }
         </div>
